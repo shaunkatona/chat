6e58f0bd18f1f79d07e08e38f96f6423052305a9
Shaun Katona <shaun.katona@markit.com>
2014-02-03 20:56:01 -0700

    all the 2.4.x changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
+var chatServer = require('./lib/chat_server');
 var cache = {};
 
 function send404(response) {
@@ -52,3 +53,5 @@ var server = http.createServer(function (request, response) {
 server.listen(3000, function () {
 	console.log("Server listening on port 3000");
 });
+
+chatServer.listen(server);
